c32cd41257021bdc5d33f8e49996edf16c21b306
hermansenph <hermansenph@gmail.com>
2018-04-03 17:50:54 -0700

    Added additional environment variables to wp-config #3

--- wp-config.php
+++ wp-config.php
@@ -49,14 +49,14 @@ define('DB_COLLATE', '');
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '/YT0p:#{0BSynX^$Y]0Y:sLM0~$#$/Ebx%T~Y/A2!~dEv4IKZ%VFaJjV,Os%#o$/');
-define('SECURE_AUTH_KEY',  '3s^A*2)Z4-5^6Eo!Dfn;E8Ig[J#GX@gM}m:M;Rft*%Jo/_Y.i`+4Nu~nf49SinXG');
-define('LOGGED_IN_KEY',    '@$MrQUi?4t`2lMKU6xk6<2j~7RAvi+Kv,*nH&g(0!H^v!$%(o,>RBm{8ZRxbf0Rj');
-define('NONCE_KEY',        '7ciXF!>es8e|sum#s7(Xy?Z-y{YyxoH)M0{g}kXIiNUW[Bf:i(6L,F*hG4dnJF4W');
-define('AUTH_SALT',        'wE3Yi.DMmRO_j{k8<uG:QjJG[q|o]fRHT%T2]QF$zXwM7I;J>rY)?0g{uxu75+=~');
-define('SECURE_AUTH_SALT', 'd)i9`0^<BNb+=mDq3.C~iG}IDZl8O4:4D|9u/4usMj&Gw$13uFsjxD:+c(VJB2)Y');
-define('LOGGED_IN_SALT',   '^dAP&{$qjXyq49XDBM~WFWS~+I]0.u/5I&uOTI/1[3ze4K1U{@A91!^NK-4GEvvT');
-define('NONCE_SALT',       'sB+qZqlJO6UJ6?~Ma/!~S^R2;Iekmt5|i:eMOV;4Pb,qO&!i^kUSqkY7{S4XIXtw');
+define('AUTH_KEY', getenv('AUTH_KEY'));
+define('SECURE_AUTH_KEY', getenv('SECURE_AUTH_KEY'));
+define('LOGGED_IN_KEY', getenv('LOGGED_IN_KEY'));
+define('NONCE_KEY', getenv('NONCE_KEY'));
+define('AUTH_SALT', getenv('AUTH_SALT'));
+define('SECURE_AUTH_SALT', getenv('SECURE_AUTH_SALT'));
+define('LOGGED_IN_SALT', getenv('LOGGED_IN_SALT'));
+define('NONCE_SALT', getenv('NONCE_SALT'));
 
 /**#@-*/
 
